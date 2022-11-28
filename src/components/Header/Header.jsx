1424c2b0043328a416d4484d4f6821230d740cf3
Ivan <vanish8ii@gmail.com>
2022-11-28 20:19:49 +0300

    Создан шаблон шапки, реализовано переключение темы

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from 'react';
+import styles from './Header.module.scss';
+
+import { IoMoon, IoMoonOutline } from 'react-icons/io5';
+import Container from '../Container/Container';
+
+const Header = () => {
+  const [theme, setTheme] = useState('light');
+
+  const toggleTheme = () => setTheme(theme === 'light' ? 'dark' : 'light');
+
+  useEffect(() => {
+    document.body.setAttribute('data-theme', theme);
+  }, [theme]);
+
+  return (
+    <header className={styles.header}>
+      <Container>
+        <div className={styles.wrapper}>
+          <a className={styles.title} href="/">
+            Punk Beer
+          </a>
+          <div className={styles.modeSwitcher} onClick={toggleTheme}>
+            <IoMoon /> Light Theme
+          </div>
+        </div>
+      </Container>
+    </header>
+  );
+};
+
+export default Header;
