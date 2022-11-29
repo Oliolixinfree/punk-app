ddfcfdd166ed3f36e35bff67890caa1bec78baaa
Ivan <offlane@tut.by>
2022-11-30 01:16:12 +0300

    react-select, стили селекта

--- src/components/Controls/Controls.jsx
+++ src/components/Controls/Controls.jsx
@@ -1,13 +1,24 @@
 import React, { useState } from 'react';
+import CustomSelect from '../CustomSelect/CustomSelect';
 import Search from '../Search/Search';
 import styles from './Controls.module.scss';
 
+export const options = [
+  { value: 'Test1', label: 'Test1' },
+  { value: 'Test2', label: 'Test2' },
+  { value: 'Test3', label: 'Test3' },
+  { value: 'Test4', label: 'Test4' },
+];
+
 const Controls = () => {
   const [search, setSearch] = useState('');
+  // const [something, setSomething] = useState('');
+
   return (
-    <>
+    <div className={styles.wrapper}>
       <Search search={search} setSearch={setSearch} />
-    </>
+      <CustomSelect options={options} />
+    </div>
   );
 };
 
