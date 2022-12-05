9c0bdfa3d02c7e9a8d37c04cbbc9c0d63b73feeb
Ivan <offlane@tut.by>
2022-12-05 21:17:18 +0300

    Настройка роутинга

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,7 +2,10 @@ import React, { useEffect, useState } from 'react';
 import styles from './Header.module.scss';
 
 import { IoMoon, IoMoonOutline } from 'react-icons/io5';
+import { IoIosBeer } from 'react-icons/io';
 import Container from '../Container/Container';
+import { Link } from 'react-router-dom';
+// import { ReactComponent as Logo } from '../../assets/logo.svg';
 
 const Header = () => {
   const [theme, setTheme] = useState('light');
@@ -17,9 +20,11 @@ const Header = () => {
     <header className={styles.header}>
       <Container>
         <div className={styles.wrapper}>
-          <a className={styles.title} href="/">
-            Punk Beer
-          </a>
+          <Link className={styles.title} to="/">
+            <IoIosBeer size={'35px'} />
+            {/* <Logo width={'18px'} height={'40px'} /> */}
+            <span>Punk Beer</span>
+          </Link>
           <div className={styles.modeSwitcher} onClick={toggleTheme}>
             {theme === 'light' ? <IoMoonOutline size={'16px'} /> : <IoMoon size={'16px'} />}
             {theme} Theme
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
+import { GET_ALL_BEERS } from '../config';
+import Controls from '../components/Controls/Controls';
+import List from '../components/List/List';
+import Card from '../components/Card/Card';
+import { useNavigate } from 'react-router-dom';
+
+const HomePage = ({ beers, setBeers }) => {
+  // const [beers, setBeers] = useState([]);
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    try {
+      if (!beers.length) {
+        axios.get(GET_ALL_BEERS).then(({ data }) => setBeers(data));
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
+
+  return (
+    <>
+      <Controls />
+      <List>
+        {beers.map((b) => {
+          const beerInfo = {
+            img: b.image_url,
+            name: b.name,
+            // description: b.description,
+            tagline: b.tagline,
+            // abv: b.abv,
+            // ibu: b.ibu,
+            // ebc: b.ebc,
+            info: [
+              {
+                title: 'ABV',
+                description: b.abv,
+              },
+              {
+                title: 'IBU',
+                description: b.ibu,
+              },
+              {
+                title: 'EBC',
+                description: b.ebc,
+              },
+            ],
+          };
+
+          return <Card key={b.name} onClick={() => navigate(`/beer/${b.name}`)} {...beerInfo} />;
+        })}
+      </List>
+    </>
+  );
+};
+
+export default HomePage;
